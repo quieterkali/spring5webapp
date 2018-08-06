62abd5c15f8b50da288ba8240c2332670e145885
Daouda Traore <quieterkali@gmail.com>
2018-08-05 22:27:08 -0300

    WIP thymeleaf and controller

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -6,7 +6,6 @@ import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
 import guru.springframework.spring5webapp.repositories.PublisherRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
